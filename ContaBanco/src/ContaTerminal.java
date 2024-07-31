e20e92fa46b6f88f541d3b9b479a306760d9adcc
LuizGusta1 <luiz.gustavobr2602@gmail.com>
2024-07-31 20:51:51 -0300

    Implementação do projeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o seu nome: ");
+        String nome_cliente = input.nextLine();
+
+        System.out.println("Digite o numero da conta: ");
+        int numero_conta = input.nextInt();
+
+        System.out.println("Digite o numero da Agência: ");
+        String numero_agencia = input.next();
+
+        System.out.println("Digite o valor do seu Saldo: ");
+        double saldo = input.nextDouble();
+
+        System.out.println("Olá " + nome_cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numero_agencia + ", conta " + numero_conta + " e seu saldo " + saldo + " já está disponivel para saque.");
+   
+        input.close();
     }
 }
